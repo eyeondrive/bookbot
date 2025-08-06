f88f0dd3ff8d290fd6378902d8f8163299e2e200
David Powell <david@chavurot.com>
2025-08-06 17:02:46 -0400

    more small changes that succeeded

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ def main():
     from stats import get_num_words, get_num_letters
     import sys
     #filepath = "./books/frankenstein.txt"
-    print(sys.argv[0])
+    #print(sys.argv[0])
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     else:
         for i in range(1,len(sys.argv)):
             filepath = sys.argv[i]
-            #print(f"this is book file {filepath}")
+            print(f"this is book file {filepath}")
             output_words = get_num_words(filepath)
             output_letters = get_num_letters(filepath)
             print("============ BOOKBOT ============")
-            print("Analyzing book found at books/frankenstein.txt...")
+            print(f"Analyzing book found at {filepath}...")
             print("----------- Word Count ----------")
             print(f"Found {output_words} total words")
             print("--------- Character Count -------")
